b195f64c9aae61cc8bc694b79a4dd77eb6382c0b
Vlessa <vlessa901@gmail.com>
2023-09-01 14:50:48 -0300

    branch com o código da aula do dia Fri, Sep  1, 2023  2:50:48 PM

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import TelaLogin from './src/TelaLogin';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+<TelaLogin/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/TelaLogin.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import{View, Text, InputText, StyleSheet} from 'react-native';
+
+const FlexDimensionsBasics = () => {
+    return (
+       <View style={{flex: 1}}>
+       <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+       <Text style={styles.heading}> Tela de login </Text>
+       </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    heading: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBotton: 20,
+    
+    },
+});
+
+export default FlexDimensionsBasics
